684a292ef8d335625156f0bf09f83fb14dfedccd
iCarlosCode <carlospdosc@gmail.com>
2022-12-30 16:29:33 -0300

    Primeiro Layout
    Adicionei:
    - NavBar
    - Menu das Abas
    - Conteúdo das Abas
    - Caixas de diálogo: Configurações, Adicionar, Deletar, Editar, e Relatório.
    
    Esses já haviam sido feitos por @Manupedro.
    
    Co-Authored-By: Manupedro <19576733+Manupedro@users.noreply.github.com>

--- public/app.js
+++ public/app.js
@@ -1,5 +1,6 @@
 const logarComGoogleBtn = document.getElementById('logarComGoogleBtn');
 const deslogarDoGoogleBtn = document.getElementById('deslogarDoGoogleBtn');
+const configsBtn = document.getElementById('configsBtn');
 const addAluno = document.getElementById('addAluno');
 const editAluno = document.getElementById('editAluno');
 const removeAluno = document.getElementById('removeAluno');
@@ -19,19 +20,27 @@ let unsubscribe;
 
 auth.onAuthStateChanged(user => {
   if (user) {
+    //console.log(firebase)
     // Quando o usuário Logar essa parte será executada
     secaoPrincipal.hidden = false;
     secaoLogin.hidden = true;
     userDetails.innerHTML = `<h3>Hello ${user.displayName}!</h3> <p>User ID: ${user.uid}</p>`;
+    configsBtn.src = user.photoURL
+    
 
     alunosRef = db.collection('alunos');
-
+    /*setarAluno(user, alunosRef, 'Luiz Algusto', 2020123459);
+    setarAluno(user, alunosRef, 'Inácio Estácio de Sá', 2020123460);
+    setarAluno(user, alunosRef, 'Florisvalda Hortência Tulipa', 2020123463);*/
     addAluno.onclick = () => setarAluno(user, alunosRef, 'Carlos N', 2020123457);
     editAluno.onclick = () => editarAluno(user, alunosRef, 'Pedro', 2020123457, [2, 0, 0, 0]);
     removeAluno.onclick = () => removerAluno(user, alunosRef, 2020123457);
 
     // Pega os dados dos alunos cadastrados no servidor e exibe eles na tela
-    unsubscribe = exibirListaDeAlunos(user, alunosRef);
+    exibirListaDeAlunos(user, alunosRef, alunosLista1, 0);
+    exibirListaDeAlunos(user, alunosRef, alunosLista2, 1);
+    exibirListaDeAlunos(user, alunosRef, alunosLista3, 2);
+    unsubscribe = exibirListaDeAlunos(user, alunosRef, alunosLista4, 3);
 
   } else {
     //  Quando o usuário estiver deslogado essa parte será executada
@@ -99,7 +108,7 @@ function editarAluno(user, collectionRef, nome, matricula, frequencia) {
 }
 
 
-function exibirListaDeAlunos(user, collectionRef) {
+/*function exibirListaDeAlunos(user, collectionRef) {
     unsubscribe = collectionRef
     .where('uid', '==', user.uid)
     .onSnapshot(querySnapshot => {
@@ -107,6 +116,36 @@ function exibirListaDeAlunos(user, collectionRef) {
         return `<li>${doc.data().nome} \(${doc.data().matricula}\) ${doc.data().frequencia[0]}</li>`;
       });
       alunosLista.innerHTML = items.join('');
+    });
+
+    return unsubscribe
+}*/
+
+function exibirListaDeAlunos(user, collectionRef, listGroup, frequenciaIndex) {
+    unsubscribe = collectionRef
+    .where('uid', '==', user.uid)
+    .onSnapshot(querySnapshot => {
+      const items = querySnapshot.docs.map(doc => {
+        return `<a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+            <!--Dados aluno-->
+            <div style="float: left; margin-left: 10px;">
+              <span class="material-symbols-outlined">person</span>
+              <label style="vertical-align: top; max-width: 350px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">${doc.data().nome}</label>
+              <br>
+              <span class="material-symbols-outlined">money</span>
+              <label style="vertical-align: top;">${doc.data().matricula}</label>
+            </div>
+            
+            <!--Botão-->
+            
+            <div class="btn-group" role="group" aria-label="Basic example" style="float: right; margin-right: 10px; margin-top: 10px;">
+                <button type="button" class="btn btn-primary">-</button>
+                <button type="button" class="btn btn-primary">${doc.data().frequencia[frequenciaIndex]}</button>
+                <button type="button" class="btn btn-primary">+</button>
+            </div>
+          </a>`;
+      });
+      listGroup.innerHTML = items.join('');
     });
 
     return unsubscribe
