7674b3c751428d43f546ecafe4954aebe6816b43
MADARINHA <n-arlan12@hotmail.com>
2023-01-26 22:43:02 -0300

    Update app.js

--- public/app/app.js
+++ public/app/app.js
@@ -684,10 +684,13 @@ function mudarEstadosDaInterfaceNaSelecao(n, index) {
     buttonsExtra[2].hidden = !ativacao;
   }
 
+  telaLimpa()
+
   if (itensSelecionadosListGroup.length == 0 && history.state.id == 'selecao')
   {
       history.go(-1);
   }
+  
 }
 
 function configurarDialogPushState(id, dialog_id, url) {
@@ -1227,7 +1230,23 @@ function exibirListaDeAlunos(user, collectionRef, listGroup, frequenciaIndex) {
         escolherFunc()//exibe a lista de acordo com a barra de pesquisa
       }
     });
+    telaLimpa()
   return unsubscribe;
+}
+
+function telaLimpa(){
+  /*console.log($('.list-group-item'))
+  if($('.list-group-item').length == 0){
+    var buttonsPadrão = document.querySelectorAll('.buttonsPadrão');
+    var buttonsExtra = document.querySelectorAll('.buttonsExtra');
+
+    for (b = 0; b < buttonsPadrão.length; b++) {
+      buttonsPadrão[b].hidden = true;
+    }
+    for (b = 0; b < buttonsExtra.length; b++) {
+      buttonsExtra[b].hidden = true;
+    }
+  }*/
 }
 
 function monitorarEstadoChamadaVirtual(user, collectionRef) {
